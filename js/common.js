a24bb00f2c9aac9cb3d8ce727d5c17839dbc6dc3
Sonali Dipsikha <sonalidipsikha43@outlook.com>
2017-10-09 09:37:30 +0530

    expand url

--- js/common.js
+++ js/common.js
@@ -22,8 +22,25 @@ UrlShorteningApp.factory("UrlShorteningfactory", ["$q", "$http", function ($q, $
 
     }])
 
+UrlShorteningApp.factory("UrlLongeringfactory", ["$q", "$http", function ($q, $http) {
+        var deferred = $q.defer();
 
-UrlShorteningApp.controller("UrlShortening", ["$scope", "$http", "UrlShorteningfactory", function ($scope, $http, UrlShorteningfactory) {
+        return{
+            longerUrl: function (shortUrl) {
+                return  $http.get('https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyCq5fBz2eo3gBgCpVM_-CUu4OClLEe48ow&shortUrl=' + shortUrl).success(function (data) {
+                    deferred.resolve(data);
+                }).
+                        error(function (error) {
+                            alert("Unable to crete actual  URL")
+                        });
+                return deferred.promise;
+            }
+        }
+
+    }])
+
+
+UrlShorteningApp.controller("UrlShortening", ["$scope", "$http", "UrlShorteningfactory","UrlLongeringfactory", function ($scope, $http, UrlShorteningfactory,UrlLongeringfactory) {
         $scope.urlShow = false;
         $scope.checkUrl = function (longUrl) {
             console.log($scope.userForm.longUrl.$invalid,$scope.longUrl);
@@ -33,12 +50,30 @@ UrlShorteningApp.controller("UrlShortening", ["$scope", "$http", "UrlShorteningf
             }
                 
         }
+         $scope.checkUrlShort = function (sortUrl) {
+            if ((sortUrl === null) || ($scope.userForm.shortUrlCreate.$invalid))
+            {
+                $scope.urlShowLong = false;
+            }
+                
+        }
 
         $scope.UrlShortening = function (longUrl) {
             UrlShorteningfactory.shortenUrl(longUrl).success(function (data) {
                 $scope.shortUrl = data.id;
                 if(data)
                 $scope.urlShow = true;
+            }).
+                    error(function (error) {
+                        $scope.error = error.error.message;
+                    })
+        }
+        
+         $scope.UrlLongering = function (shortUrl) {
+            UrlLongeringfactory.longerUrl(shortUrl).success(function (data) {
+                $scope.longerUrl=data.longUrl;
+                if(data.longUrl)
+                $scope.urlShowLong = true;
             }).
                     error(function (error) {
                         $scope.error = error.error.message;
